90fabc3c6201b45c2a876ed784dc763a0091d261
luckyrat <luckyrat@musites.com>
2023-08-08 09:39:40 +0100

    Stop using DPIScaledToolStripMenuItems
    Appears to be no longer necessary in recent KeePass versions.
    
    Fixes #77

--- KeePassRPC/Properties/AssemblyInfo.cs
+++ KeePassRPC/Properties/AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Runtime.InteropServices;
 
 // Assembly version information
 [assembly: AssemblyVersion("2.0.48.*")]
-[assembly: AssemblyFileVersion("1.15.0.0")] // also change PluginVersion in KeePassRPCExt.cs!
+[assembly: AssemblyFileVersion("1.15.1.0")] // also change PluginVersion in KeePassRPCExt.cs!
